896ef1bd5a817a29411b5872df33cc171d12352d
Tanish Solanki <tanishs4313@gmail.com>
2025-06-02 03:03:45 +0530

    refactor(endpoint): make changes to optimaize the routes
    d

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -66,7 +66,7 @@ app.get(
   '/login',
   async (req: Request<{}, {}, {}, LoginQuery>, res: Response): Promise<any> => {
     const { email, password } = req.query;
-    const user = await UserModel.findOne({ email: email });
+    const user = await UserModel.findOne({ email: email }).select({ password: 1 }).lean();
     if (!user || password != user.password) {
       return res.status(401).json({ error: "Invalid email or password" });
     }
@@ -79,28 +79,26 @@ app.post(
   async (req: Request<{}, {}, SignUpBody>, res: Response): Promise<any> => {
     const { username, email, password, image_uri } = req.body;
 
-    const isVaild = await UserModel.findOne({ email: email });
+    const isVaild = await UserModel.exists({ email: email });
     if (isVaild) {
       return res.status(400).json({ error: "Email Already exisits" });
     }
-    const user = new UserModel({ username: username, email: email, password: password, image_uri: image_uri });
-    await user.save();
+    const user = await UserModel.create({ username, password, email, image_uri });
     res.json({ message: 'Signup successful', user });
   }
 );
 
-
 // /blog/:blogid (get)
 app.get(
   '/blog/:blogId',
+  // TODO: clean the response of this endpoint 
   async (req: Request<{ blogId: string }>, res: Response): Promise<any> => {
     const { blogId } = req.params;
-    const blog = await BlogModel.findById(blogId);
-
+    const blog = await BlogModel.findById(blogId).lean();
     if (!blog) {
       return res.status(404).json({ error: "Blog Not Found" });
     }
-    const user = await UserModel.findById(blog.userId);
+    const user = await UserModel.findById(blog.userId).lean();
     res.json({ blog, user });
   }
 );
@@ -111,20 +109,16 @@ app.get(
   '/blogs/:userId',
   async (req: Request<{ userId: string }>, res: Response): Promise<any> => {
     const { userId } = req.params;
-    const blogs = await BlogModel.find({ userId: userId });
-    const user = await UserModel.findById(userId);
+    const blogs = await BlogModel.find({ userId: userId }).populate("userId", "username").lean();
 
     if (!blogs) {
       return res.status(404).json({ error: "Blog Not Found" });
     }
-    if (!user) {
-      return res.status(404).json({ error: "User Not Found" });
-    }
-    let resBlogs: BlogResponse[] = blogs.map(({ _id ,title, shortDescription, creationDate, image_uri }) => {
+    let resBlogs: BlogResponse[] = blogs.map(({ _id, title, shortDescription, creationDate, image_uri, userId }) => {
       return <BlogResponse>{
         blogId: _id.toString(),
         title,
-        author: user.username,
+        author: (userId as any).username,
         shortDescription,
         creationDate,
         image_uri
@@ -139,27 +133,22 @@ app.get(
 app.get(
   '/blogs/',
   async (_req: Request, res: Response): Promise<any> => {
-    const blogs = await BlogModel.find();
+    const blogs = await BlogModel.find().populate("userId", "username");
     if (!blogs) {
       return res.status(404).json({ error: "Blog Not Found" });
     }
-    let resBlogs: BlogResponse[] = (await Promise.all(
-      blogs.map(async ({ _id, title, shortDescription, userId, creationDate, image_uri }): Promise<BlogResponse | null> => {
-        const user = await UserModel.findById(userId).select({ username: 1 });
-        if (!user) return null;
+    let resBlogs: BlogResponse[] = blogs.map(({ _id, title, shortDescription, userId, creationDate, image_uri }): BlogResponse => {
+      return <BlogResponse>{
+        blogId: _id.toString(),
+        title,
+        author: (userId as any).username,
+        shortDescription,
+        creationDate,
+        image_uri
+      };
+    });
 
-        return <BlogResponse>{
-          blogId: _id.toString(),
-          title,
-          author: user.username,
-          shortDescription,
-          creationDate,
-          image_uri
-        };
-      })
-    )).filter((blog) => blog != null);
-
-    res.json({ blogs: resBlogs });
+res.json({ blogs: resBlogs });
   }
 );
 
@@ -170,7 +159,7 @@ app.post(
     const shortDescription = generateShortDescription(content);
     const blog = new BlogModel({ title, content, image_uri, userId, shortDescription });
     await blog.save();
-    res.json({ message: "blog saved"});
+    res.json({ message: "blog saved" });
   }
 )
 
@@ -193,7 +182,7 @@ app.post(
     }
     if (image_uri) blog.image_uri = image_uri;
 
-    res.json({ message: "updated Successfull"})
+    res.json({ message: "updated Successfull" })
   }
 )
 app.get(
@@ -202,7 +191,7 @@ app.get(
     const { blogId } = req.params;
     const deleted = await BlogModel.findByIdAndDelete(blogId);
     if (!deleted) return res.status(404).json({ error: 'Blog not found' });
-    res.json({ message: 'Blog deleted'});
+    res.json({ message: 'Blog deleted' });
   }
 )
 
